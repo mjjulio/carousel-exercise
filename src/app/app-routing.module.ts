15c03b877aeccd1ea2b03ef143fd16ad5b2606e5
mjjulio <mjjulio@yahoo.com>
2021-04-13 15:57:40 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselTinySliderComponent } from './carousel-tiny-slider/carousel-tiny-slider.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'glidejs', component: CarouselComponent },
+  { path: 'tinyslider', component: CarouselTinySliderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
